ba37604a62a36f810768fa7f0339847262415ed9
Nick O'Neill <nick@gone.me.uk>
2025-03-17 18:04:03 +0000

    added function for listing devices with shapers less than 200Mb useful for checking QoS uplifts match config

--- sdwan-devices.py
+++ sdwan-devices.py
@@ -30,6 +30,95 @@ from pprint import pprint
 import urllib3
 import sys
 
+def qosreport(routers):
+
+    shaperdict = {}
+    shaperdict["hostname"] = []
+    shaperdict["ip"] = []
+    shaperdict["shaperate"] = []
+
+    for router in routers:
+        reachable=str(router.reachability)
+        systemip = router.id
+        hostname = router.hostname
+        
+        if "UNREACHABLE" in reachable:
+            print(f"{systemip:<20}{hostname:<35}{'Device offline - Skipping'}")
+            continue
+
+        print(f"{systemip:<20}{hostname:<35}{'Connecting...'}")
+        
+        try:
+            ssh_connect = ConnectHandler(host=systemip, username=ciscouser, password=ciscopass, device_type="cisco_ios")
+		# Some error
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+            continue
+        
+        shapertext = ssh_connect.send_command("sh policy-map int output | inc target shape rate",use_textfsm=False)
+        if len(shapertext) == 0:
+			# no shaper
+            continue
+        shaperate = int(shapertext.split("target shape rate ")[1])
+        shaperate = int(shaperate/1000000)
+        if shaperate < 200:
+            print(f"{shaperate}Mb")
+            shaperdict["hostname"].append(hostname)
+            shaperdict["ip"].append(systemip)
+            shaperdict["shaperate"].append(shaperate)
+
+    print("\nThe following shapers of less than 200Mb were found:\n\n")
+
+    for index in range(len(shaperdict["hostname"])):
+        print(f'{shaperdict["ip"][index]:<20}{shaperdict["hostname"][index]:<35}{shaperdict["shaperate"][index]}Mb')
+
+            
+
+def disableportscheck(routers):
+    change_required = []
+
+    print("\fChecking devices to see if ports need disabling...\n\n")
+
+    for router in routers:
+        if "1127" in router.uuid or "1161" in router.uuid:
+            reachable=str(router.reachability)
+            systemip = router.id
+            hostname = router.hostname
+            serial = router.uuid.split("-")[-1]
+            if "UNREACHABLE" in reachable:
+                print(f"{systemip:<20}{hostname:<35}{'Device offline - Skipping'}")
+                continue
+            print(f"{systemip:<20}{hostname:<35}{'Connecting...'}")
+            try:
+                ssh_connect = ConnectHandler(host=systemip, username=ciscouser, password=ciscopass, device_type="cisco_ios")
+        
+            # Some error
+            except Exception as e:
+                print(f"An error occurred: {str(e)}")
+                continue
+        
+
+            interface_status = ssh_connect.send_command("show interface status",use_textfsm=True)
+            #print(f'{interface_status[0]["port"]} {interface_status[0]["status"]}')
+            #print(f'{interface_status[4]["port"]} {interface_status[3]["status"]}')
+            if interface_status[0]["status"] == "connected":
+                if interface_status[4]["status"] != "disabled":
+                    print(f"{serial:<55}{'Needs ports disabling'}")
+                    change_required.append(serial)
+            ssh_connect.disconnect()
+
+    # Print device serials requiring change
+
+    change_required = set(change_required)
+
+    if not change_required:
+        print("No changes required!")
+        sys.exit()
+
+    print("\nThe following serial numbers need the provision ports disabling:\n\n")
+    for serial in change_required:
+        print(serial)
+
 username = input("Enter your vManage username: ")
 password = pwinput("Enter your vManage password: ")
 
@@ -58,55 +147,26 @@ except ManagerHTTPError as error:
 
 # Get devices and filter on EDGES, then loop through and pick out the 1100s used for NGN (1127 and 1161)
 
-print("\fChecking devices...\n\n")
-
 devices = session.api.devices.get()
 routers = devices.filter(personality=Personality.EDGE)
 
-change_required = []
+while True:
+    print("\n\nUtility for interacting with devices in vManage")
+    print("-----------------------------------------------\n")
+    print("[1] Check for provisoning ports that should be disabled")
+    print("[2] List sites with shapers less than 200Mb")
+    print("[3] Exit this utility")
+    choice = int(input("\n Please select an option and press [ENTER]:"))
 
-for router in routers:
-    if "1127" in router.uuid or "1161" in router.uuid:
-        reachable=str(router.reachability)
-        systemip = router.id
-        hostname = router.hostname
-        serial = router.uuid.split("-")[-1]
-        if "UNREACHABLE" in reachable:
-            print(f"{systemip:<20}{hostname:<35}{'Device offline - Skipping'}")
-            continue
-        print(f"{systemip:<20}{hostname:<35}{'Connecting...'}")
-        try:
-            ssh_connect = ConnectHandler(host=systemip, username=ciscouser, password=ciscopass, device_type="cisco_ios")
-      
-        # Some error
-        except Exception as e:
-            print(f"An error occurred: {str(e)}")
-            continue
+    if choice == 1:
+        disableportscheck(routers)
     
+    if choice ==2:
+        qosreport(routers)
 
-        interface_status = ssh_connect.send_command("show interface status",use_textfsm=True)
-        #print(f'{interface_status[0]["port"]} {interface_status[0]["status"]}')
-        #print(f'{interface_status[4]["port"]} {interface_status[3]["status"]}')
-        if interface_status[0]["status"] == "connected":
-            if interface_status[4]["status"] != "disabled":
-                print(f"{serial:<55}{'Needs ports disabling'}")
-                change_required.append(serial)
-        ssh_connect.disconnect()
-    
-# Close the vManage session
+    if choice == 3:
+    # Close the vManage session
 
-print("\nClosing the vManage session")
-
-session.close()
-
-# Print device serials requiring change
-
-change_required = set(change_required)
-
-if not change_required:
-    print("No changes required!")
-    sys.exit()
-
-print("\nThe following serial numbers need the provision ports disabling:\n\n")
-for serial in change_required:
-    print(serial)
+        print("\nClosing the vManage session")
+        session.close()
+        sys.exit()
