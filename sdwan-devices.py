c9367c5dc8ce3a4fbbc635de63fe6cc57e517ed8
Nick O'Neill <nick@gone.me.uk>
2025-03-18 17:46:47 +0000

    work in progress on QoS report which grabs outbound shaper and downstream bandwidth requested at hubs

--- sdwan-devices.py
+++ sdwan-devices.py
@@ -8,7 +8,7 @@ from pwinput import pwinput
 
 
 # SSH library
-from netmiko import ConnectHandler
+from netmiko import ConnectHandler,NetmikoTimeoutException
 
 # Cisco SDK for Catlayst SDWAN
 from catalystwan.session import create_manager_session
@@ -29,18 +29,28 @@ from catalystwan.exceptions import (
 from pprint import pprint
 import urllib3
 import sys
+import logging
 
 def qosreport(routers):
 
+    logging.basicConfig(filename="netmiko.log", level=logging.DEBUG)
+    logger = logging.getLogger("netmiko")
+    
     shaperdict = {}
     shaperdict["hostname"] = []
     shaperdict["ip"] = []
     shaperdict["shaperate"] = []
+    shaperdict["downstream"] = []
+    shaperdict["live"] = []
+
+    total_devices = 0
 
     for router in routers:
         reachable=str(router.reachability)
         systemip = router.id
         hostname = router.hostname
+
+        total_devices = total_devices + 1
         
         if "UNREACHABLE" in reachable:
             print(f"{systemip:<20}{hostname:<35}{'Device offline - Skipping'}")
@@ -61,21 +71,55 @@ def qosreport(routers):
             continue
         shaperate = int(shapertext.split("target shape rate ")[1])
         shaperate = int(shaperate/1000000)
-        if shaperate < 200:
-            print(f"{shaperate}Mb")
-            shaperdict["hostname"].append(hostname)
-            shaperdict["ip"].append(systemip)
-            shaperdict["shaperate"].append(shaperate)
+        if shaperate > 100: continue
+
+        # shaperate is 100Mb or less so collect additional info
+        print(f"{shaperate}Mb")
+        shaperdict["hostname"].append(hostname)
+        shaperdict["ip"].append(systemip)
+        shaperdict["shaperate"].append(shaperate)
+        interface_status = ssh_connect.send_command("show interface status",use_textfsm=True)
+        if interface_status[0]["status"] == "connected" or interface_status[1]["status"] =="connected":
+            shaperdict["live"].append("Live")
+        else:
+            shaperdict["live"].append("Not Live")
+        
+        # get downstream bandwidth
+        #try:
+        #    downstreamtext = ssh_connect.send_command("sh sdwan run | inc downst",read_timeout=30,use_textfsm=False)
+        #except NetmikoTimeoutException:
+        #    shaperdict["downstream"].append("timeout")
+        #   continue
+
+        # get WAN IP
+        ceftext = ssh_connect.send_command("sh ip cef 0.0.0.0/0 | inc nexthop",use_textfsm=False)
+        wanif = (ceftext.split(" ")[-1])
+        protocoltext = ssh_connect.send_command("sh prot "+wanif+" | inc Internet address",use_textfsm=False)
+        protocoltext = protocoltext.split("/")[0]
+        wanip = protocoltext.split(" ")[-1]
+
+        # connect to a hub and read the downstream bandwidth that was requested
+
+
+        if len(downstreamtext) == 0:
+            shaperdict["downstream"].append("not configured")
+            continue
+        downrate = int(int(downstreamtext.split("bandwidth-downstream ")[1])/1000)
+        shaperdict["downstream"].append(str(downrate)+" Mb")
+
+    print(f"\nTotal devices (including unreacahble): {total_devices}")        
 
     print("\nThe following shapers of less than 200Mb were found:\n\n")
 
     for index in range(len(shaperdict["hostname"])):
-        print(f'{shaperdict["ip"][index]:<20}{shaperdict["hostname"][index]:<35}{shaperdict["shaperate"][index]}Mb')
+        print(f'{shaperdict["ip"][index]:<20}{shaperdict["hostname"][index]:<35}up:{shaperdict["shaperate"][index]:<3}Mb    down:{shaperdict["downstream"][index]:<16} Migrated?:{shaperdict["live"][index]}')
 
             
 
 def disableportscheck(routers):
+    
     change_required = []
+    total_devices = 0
 
     print("\fChecking devices to see if ports need disabling...\n\n")
 
@@ -85,6 +129,7 @@ def disableportscheck(routers):
             systemip = router.id
             hostname = router.hostname
             serial = router.uuid.split("-")[-1]
+            total_devices = total_devices + 1
             if "UNREACHABLE" in reachable:
                 print(f"{systemip:<20}{hostname:<35}{'Device offline - Skipping'}")
                 continue
@@ -111,6 +156,8 @@ def disableportscheck(routers):
 
     change_required = set(change_required)
 
+    print(f"\nTotal devices 1127 and 1161 only (including unreacahble): {total_devices}") 
+
     if not change_required:
         print("No changes required!")
         sys.exit()
@@ -125,7 +172,7 @@ password = pwinput("Enter your vManage password: ")
 ciscouser = input("\nEnter your ISE username: ")
 ciscopass = pwinput("Enter your ISE password: ")
 
-proceed = input("\nNOTE: Ensure you have an active VPN connection for management before proceeding [ENTER]:")
+proceed = input("\nNOTE: Ensure you have an active VPN connection for management before proceeding [ENTER]: ")
 
 
 base_url = "https://vmanage-171203704.sdwan.cisco.com/"
@@ -156,7 +203,9 @@ while True:
     print("[1] Check for provisoning ports that should be disabled")
     print("[2] List sites with shapers less than 200Mb")
     print("[3] Exit this utility")
-    choice = int(input("\n Please select an option and press [ENTER]:"))
+    choice = int(input("\n Please select an option and press [ENTER]: "))
+
+    print("\n")
 
     if choice == 1:
         disableportscheck(routers)
@@ -167,6 +216,6 @@ while True:
     if choice == 3:
     # Close the vManage session
 
-        print("\nClosing the vManage session")
+        print("Closing the vManage session")
         session.close()
         sys.exit()
